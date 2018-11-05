70704ab76db8a26b6d66467adeafe4429668ec6e
noobdadepressao <victorschnepper@gmail.com>
2018-11-05 13:37:28 +0100

    merging conflicts

--- org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/gui/views/IFLMainView.java
+++ org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/gui/views/IFLMainView.java
@@ -1,6 +1,5 @@
 package org.eclipse.sed.ifl.gui.views;
 
-
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
@@ -11,32 +10,23 @@ import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.action.*;
 import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.SWT;
-
-import java.util.List;
-
 import javax.inject.Inject;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.TableColumn;
 
-
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
  * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
  * <p>
  */
 
@@ -49,19 +39,21 @@ public class IFLMainView extends ViewPart {
 	 */
 	public static final String ID = "org.eclipse.sed.ifl.views.IFLMainView";
 
-	@Inject IWorkbench workbench;
+	@Inject
+	IWorkbench workbench;
 	private Table table;
-	 
 
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 		@Override
 		public String getColumnText(Object obj, int index) {
 			return getText(obj);
 		}
+
 		@Override
 		public Image getColumnImage(Object obj, int index) {
 			return getImage(obj);
 		}
+
 		@Override
 		public Image getImage(Object obj) {
 			return workbench.getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
@@ -70,19 +62,19 @@ public class IFLMainView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		
+
 		table = new Table(parent, SWT.BORDER | SWT.FULL_SELECTION);
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
-		
+
 		TableColumn tblclmnId = new TableColumn(table, SWT.NONE);
 		tblclmnId.setWidth(100);
 		tblclmnId.setText("Name");
-		
+
 		TableColumn tblclmnSigniture = new TableColumn(table, SWT.NONE);
 		tblclmnSigniture.setWidth(100);
 		tblclmnSigniture.setText("Signiture");
-		
+
 		TableColumn tblclmnReturnType = new TableColumn(table, SWT.NONE);
 		tblclmnReturnType.setWidth(100);
 		tblclmnReturnType.setText("Return type");
@@ -91,7 +83,7 @@ public class IFLMainView extends ViewPart {
 		hookDoubleClickAction();
 		contributeToActionBars();
 	}
-	
+
 	public void setMethodList(List<IMethod> list) {
 		table.setData(list);
 		for (IMethod method : list) {
@@ -130,7 +122,7 @@ public class IFLMainView extends ViewPart {
 		// Other plug-ins can contribute there actions here
 		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 	}
-	
+
 	private void fillLocalToolBar(IToolBarManager manager) {
 	}
 
