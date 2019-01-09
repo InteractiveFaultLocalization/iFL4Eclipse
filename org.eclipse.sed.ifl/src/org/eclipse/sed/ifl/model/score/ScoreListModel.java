d60609fd0f2aab7e1ae556f632cdacdc57d4ebb9
Gergő Balogh <geryxyz@gmail.com>
2019-01-09 14:43:54 +0100

    scorelist modell cleaned

--- org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/model/score/ScoreListModel.java
+++ org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/model/score/ScoreListModel.java
@@ -1,14 +1,8 @@
 package org.eclipse.sed.ifl.model.score;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-
 import org.eclipse.sed.ifl.control.score.Score;
 import org.eclipse.sed.ifl.model.EmptyModel;
 import org.eclipse.sed.ifl.model.source.IMethodDescription;
@@ -25,7 +19,7 @@ public class ScoreListModel extends EmptyModel {
 
 	private Map<IMethodDescription, Score> scores = new HashMap<>();
 
-	public Map<IMethodDescription, Defineable<Double>> getScores() {
+	public Map<IMethodDescription, Score> getScores() {
 		return Collections.unmodifiableMap(scores);
 	}
 
@@ -42,38 +36,12 @@ public class ScoreListModel extends EmptyModel {
 				}
 			}
 		}
-		scoresUpdated.invoke(getScores());
+		scoreUpdated.invoke(getScores());
 	}
 
-	/**
-	 * @deprecated for testing only
-	 */
-	@Deprecated
-	public List<Entry<IMethodDescription, Defineable<Double>>> getRandomMethods(int count) {
-		var keyList = new ArrayList<>(scores.entrySet());
-		Collections.shuffle(keyList);
-		return keyList.stream().limit(Math.min(keyList.size(), count)).collect(Collectors.toList());
-	}
+	private NonGenericListenerCollection<Map<IMethodDescription, Score>> scoreUpdated = new NonGenericListenerCollection<>();
 
-	private NonGenericListenerCollection<Map<IMethodDescription, Defineable<Double>>> scoresUpdated = new NonGenericListenerCollection<>();
-
-	public INonGenericListenerCollection<Map<IMethodDescription, Defineable<Double>>> eventScoreUpdated() {
-		return scoresUpdated;
-	}
-
-	public int requestScoreUpdate(Map<String, Double> rawScores) {
-		int count = 0;
-		Map<IMethodDescription, Defineable<Double>> entries = new HashMap<>();
-		for (var raw : rawScores.entrySet()) {
-			for (var entry : scores.entrySet()) {
-				if (entry.getKey().getId().toCSVKey().equals(raw.getKey())) {
-					entries.put(entry.getKey(), new Defineable<>(raw.getValue()));
-					count++;
-				}
-			}
-		}
-		scoreUpdateRequested.invoke(entries);
-		System.out.println(count + "/" + scores.size() + " scores will be updated");
-		return count;
+	public INonGenericListenerCollection<Map<IMethodDescription, Score>> eventScoreUpdated() {
+		return scoreUpdated;
 	}
 }
