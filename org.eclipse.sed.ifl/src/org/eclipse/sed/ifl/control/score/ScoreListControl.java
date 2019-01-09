3f91980209ccfc02deb91c8daae58fdb2c8dd451
Gergő Balogh <geryxyz@gmail.com>
2019-01-09 15:32:49 +0100

    scorelist controler basic cleaning

--- org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreListControl.java
+++ org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreListControl.java
@@ -3,7 +3,6 @@ package org.eclipse.sed.ifl.control.score;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import org.eclipse.sed.ifl.control.Control;
 import org.eclipse.sed.ifl.core.BasicIflMethodScoreHandler;
 import org.eclipse.sed.ifl.model.score.ScoreListModel;
@@ -12,7 +11,6 @@ import org.eclipse.sed.ifl.model.user.identification.IUser;
 import org.eclipse.sed.ifl.model.user.interaction.IUserFeedback;
 import org.eclipse.sed.ifl.model.user.interaction.Option;
 import org.eclipse.sed.ifl.util.event.IListener;
-import org.eclipse.sed.ifl.util.wrapper.Defineable;
 import org.eclipse.sed.ifl.view.ScoreListView;
 
 public class ScoreListControl extends Control<ScoreListModel, ScoreListView> {
@@ -25,23 +23,26 @@ public class ScoreListControl extends Control<ScoreListModel, ScoreListView> {
 
 	@Override
 	public void init() {
-		getView().refreshScores(getModel().getScores(), ScoreStatus.UNDEFINED);
-		getModel().eventScoreUpdateRequested().add(scoreUpdateRequestedListener);
+		getView().refreshScores(getModel().getScores());
+		getModel().eventScoreUpdated().add(scoreUpdatedListener);
 		getView().createOptionsMenu(handler.getProvidedOptions());
 		getView().eventOptionSelected().add(optionSelectedListener);
-		handler.eventScoreUpdated().add(scoreUpdatedListener);
-		handler.loadMethodsScoreMap(getModel().getScores());
+		//TODO: reenable these after clean up
+		//handler.eventScoreUpdated().add(scoreUpdatedListener);
+		//handler.loadMethodsScoreMap(getModel().getScores());
 		super.init();
 	}
 
 	@Override
 	public void teardown() {
-		getModel().eventScoreUpdateRequested().remove(scoreUpdateRequestedListener);
+		getModel().eventScoreUpdated().remove(scoreUpdatedListener);
 		getView().eventOptionSelected().remove(optionSelectedListener);
-		handler.eventScoreUpdated().remove(scoreUpdatedListener);
+		//TODO: reenable this after clean up
+		//handler.eventScoreUpdated().remove(scoreUpdatedListener);
 		super.teardown();
 	}
 
+	//TODO: Yoda-mode :) split or move it to view
 	public enum ScoreStatus {
 		NONE(null), INCREASED("icons/up32.png"), DECREASED("icons/down32.png"), UNDEFINED("icons/undefined32.png");
 
@@ -56,110 +57,27 @@ public class ScoreListControl extends Control<ScoreListModel, ScoreListView> {
 		}
 	}
 
-	@Deprecated
-	public void updateScore(Map<IMethodDescription, Defineable<Double>> newScores) {
-		var buckets = detectStatus(newScores);
-		getModel().updateScore(buckets.values());
-		handler.loadMethodsScoreMap(getModel().getScores());
-		getView().refreshScores(filterForView(buckets));
+	//TODO: it was public, but I think it could be private
+	private void updateScore(Map<IMethodDescription, Score> newScores) {
+		//TODO: reenable this after clean up
+		//handler.loadMethodsScoreMap(getModel().getScores());
+		getView().refreshScores(filterForView(newScores));
 	}
 
-	private Map<ScoreStatus, Map<IMethodDescription, Defineable<Double>>> filterForView(
-			Map<ScoreStatus, Map<IMethodDescription, Defineable<Double>>> buckets) {
-		Map<ScoreStatus, Map<IMethodDescription, Defineable<Double>>> filtered = new HashMap<>();
+	//TODO: replace with strategy pattern
+	private Map<IMethodDescription, Score> filterForView(Map<IMethodDescription, Score> allScores) {
+		Map<IMethodDescription, Score> filtered = new HashMap<>();
 		if (hideUndefinedScores) {
-			for (var bucket : buckets.entrySet()) {
-				filtered.put(bucket.getKey(), new HashMap<>());
-				for (var item : bucket.getValue().entrySet()) {
-					if (item.getValue().isDefinit()) {
-						filtered.get(bucket.getKey()).put(item.getKey(), item.getValue());
-					}
+			for (var item : allScores.entrySet()) {
+				if (item.getValue().isDefinit()) {
+					filtered.put(item.getKey(), item.getValue());
 				}
 			}
 		}
 		else {
-			filtered = buckets;
+			filtered = allScores;
 		}
 		return filtered;
-	}
-
-	private IListener<Map<IMethodDescription, Defineable<Double>>> scoreUpdateRequestedListener = new IListener<>() {
-
-		@Override
-		public void invoke(Map<IMethodDescription, Defineable<Double>> event) {
-			updateScore(event);
-		}
-	};
-
-	@Deprecated
-	private Map<ScoreStatus, Map<IMethodDescription, Defineable<Double>>> detectStatus(
-			Map<IMethodDescription, Defineable<Double>> newScores) {
-		Map<IMethodDescription, Defineable<Double>> higherScores = new HashMap<IMethodDescription, Defineable<Double>>();
-		Map<IMethodDescription, Defineable<Double>> lowerScores = new HashMap<IMethodDescription, Defineable<Double>>();
-		Map<IMethodDescription, Defineable<Double>> unchangedScores = new HashMap<IMethodDescription, Defineable<Double>>();
-		Map<IMethodDescription, Defineable<Double>> otherScores = new HashMap<IMethodDescription, Defineable<Double>>();
-		for (var current : getModel().getScores().entrySet()) {
-			if (!newScores.containsKey(current.getKey())) {
-				unchangedScores.put(current.getKey(), current.getValue());
-			}
-		}
-		for (var newEntry : newScores.entrySet()) {
-			var current = getModel().getScores().get(newEntry.getKey());
-			if (current != null) {
-				if (current.isDefinit() && newEntry.getValue().isDefinit()) {
-					if (current.getValue() < newEntry.getValue().getValue()) {
-						higherScores.put(newEntry.getKey(), newEntry.getValue());
-					} else if (current.getValue() > newEntry.getValue().getValue()) {
-						lowerScores.put(newEntry.getKey(), newEntry.getValue());
-					} else {
-						unchangedScores.put(newEntry.getKey(), newEntry.getValue());
-					}
-				} else {
-					otherScores.put(newEntry.getKey(), newEntry.getValue());
-				}
-			} else {
-				otherScores.put(newEntry.getKey(), newEntry.getValue());
-			}
-		}
-		Map<ScoreStatus, Map<IMethodDescription, Defineable<Double>>> buckets = Map.ofEntries(
-				Map.entry(ScoreStatus.INCREASED, higherScores), Map.entry(ScoreStatus.DECREASED, lowerScores),
-				Map.entry(ScoreStatus.NONE, unchangedScores), Map.entry(ScoreStatus.UNDEFINED, otherScores));
-		return buckets;
-	}
-
-	// TODO: for testing only
-	public void updateRandomScores(int count) {
-		var methods = getModel().getRandomMethods(count);
-		var<ScoreStatus> statuses = List.of(ScoreStatus.DECREASED, ScoreStatus.INCREASED, ScoreStatus.NONE, ScoreStatus.UNDEFINED);
-		Random r = new Random();
-		Map<IMethodDescription, Defineable<Double>> newScores = new HashMap<>();
-		for (var entry : methods) {
-			var status = statuses.get(r.nextInt(statuses.size()));
-			var<Double> value = new Defineable<>(0.0);
-			if (entry.getValue().isDefinit()) {
-				switch (status) {
-				case DECREASED:
-					value.setValue(entry.getValue().getValue() - 1);
-					break;
-				case INCREASED:
-					value.setValue(entry.getValue().getValue() + 1);
-					break;
-				case NONE:
-					value.setValue(entry.getValue().getValue());
-					break;
-				case UNDEFINED:
-					value.undefine();
-					break;
-				default:
-					throw new RuntimeException("wrong test");
-				}
-			}
-			newScores.put(entry.getKey(), value);
-			System.out.printf("Changing %s from %s to %s\n", entry.getKey().getId().toString(),
-					entry.getValue().toString(), value.toString());
-		}
-		System.out.println("random score changes generated");
-		updateScore(newScores);
 	}
 
 	private Boolean hideUndefinedScores = false;
@@ -201,10 +119,10 @@ public class ScoreListControl extends Control<ScoreListModel, ScoreListView> {
 
 	};
 
-	private IListener<Map<IMethodDescription, Defineable<Double>>> scoreUpdatedListener = new IListener<Map<IMethodDescription, Defineable<Double>>>() {
+	private IListener<Map<IMethodDescription, Score>> scoreUpdatedListener = new IListener<>() {
 
 		@Override
-		public void invoke(Map<IMethodDescription, Defineable<Double>> event) {
+		public void invoke(Map<IMethodDescription, Score> event) {
 			updateScore(event);
 		}
 		
