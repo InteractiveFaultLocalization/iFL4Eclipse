31a46d642b80682b47beb66df35f1c8e67d766d1
Gergő Balogh <geryxyz@gmail.com>
2019-03-06 14:23:31 +0100

    add file res to navigation event

---
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/model/monitor/resource/File.java
@@ -0,0 +1,11 @@
+package org.eclipse.sed.ifl.model.monitor.resource;
+
+import java.util.HashMap;
+
+public class File extends Resource {
+
+	public File(String fullpath) {
+		super(fullpath, new HashMap<>());
+	}
+
+}
