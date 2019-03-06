a6d66003b4a1f4e84a396f0e81f127f3a383a9f3
Gergő Balogh <geryxyz@gmail.com>
2019-03-06 14:23:14 +0100

    add file res to navigation event

--- org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/model/monitor/event/NavigationEvent.java
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/model/monitor/event/NavigationEvent.java
@@ -3,6 +3,7 @@ package org.eclipse.sed.ifl.model.monitor.event;
 import java.util.HashMap;
 
 import org.eclipse.sed.ifl.model.monitor.resource.CodeElement;
+import org.eclipse.sed.ifl.model.monitor.resource.File;
 import org.eclipse.sed.ifl.model.monitor.resource.LineInfo;
 import org.eclipse.sed.ifl.model.source.IMethodDescription;
 
@@ -12,5 +13,6 @@ public class NavigationEvent extends Event {
 		super(new HashMap<>());
 		resources.put(new LineInfo(event.getLocation().getAbsolutePath(), event.getLocation().getBegining().getOffset()), "target");
 		resources.put(new CodeElement(event.getId().getKey()), "target");
+		resources.put(new File(event.getLocation().getAbsolutePath()), "target");
 	}
 }
