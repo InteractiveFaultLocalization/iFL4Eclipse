17eb64b960cc1581e6ab388b4fff4e55ba759fd9
Horváth Dávid <h348168@stud.u-szeged.hu>
2019-06-25 18:39:12 +0200

    adding new icons and creating custom warning dialog

--- org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/ide/gui/ScoreLoaderButton.java
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/ide/gui/ScoreLoaderButton.java
@@ -3,6 +3,7 @@ package org.eclipse.sed.ifl.ide.gui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.wb.swt.ResourceManager;
 
 import swing2swt.layout.BorderLayout;
 
@@ -14,5 +15,6 @@ public class ScoreLoaderButton extends Composite {
 		
 		Button loadButton = new Button(this, SWT.NONE);
 		loadButton.setText("Load scores...");
+		loadButton.setImage(ResourceManager.getPluginImage("org.eclipse.sed.ifl", "icons/load-button-icon.png"));
 	}
 }
---
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/ide/gui/dialogs/CustomWarningDialog.java
@@ -0,0 +1,61 @@
+package org.eclipse.sed.ifl.ide.gui.dialogs;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.wb.swt.ResourceManager;
+public class CustomWarningDialog extends Dialog {
+
+	private Image image;
+	private String title;
+	private String message;
+	
+	public CustomWarningDialog(Shell parentShell, String dialogTitle, String dialogMessage) {
+		super(parentShell);
+		this.title = dialogTitle;
+		this.message = dialogMessage;
+		this.image = ResourceManager.getPluginImage("org.eclipse.sed.ifl", "icons/warning-icon.png");
+	}
+	
+	@Override
+    protected void configureShell(Shell newShell) {
+        super.configureShell(newShell);
+        newShell.setText(title);
+    }
+	
+	protected Control createDialogArea(Composite parent) {
+		createMessageArea(parent);
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.marginHeight = 0;
+		layout.marginWidth = 0;
+		composite.setLayout(layout);
+		GridData data = new GridData(GridData.FILL_BOTH);
+		data.horizontalSpan = 2;
+		composite.setLayoutData(data);
+		return composite;
+	}
+	
+	protected Control createMessageArea(Composite composite) {
+		Label imageLabel = new Label(composite, SWT.NULL);
+		image.setBackground(imageLabel.getBackground());
+		imageLabel.setImage(image);
+
+		GridDataFactory.fillDefaults().align(SWT.CENTER, SWT.BEGINNING).applyTo(imageLabel);
+		
+		Label messageLabel = new Label(composite, SWT.NONE);
+		messageLabel.setText(message);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.BEGINNING).grab(true, false)
+			.hint(convertHorizontalDLUsToPixels(IDialogConstants.MINIMUM_MESSAGE_AREA_WIDTH),SWT.DEFAULT).applyTo(messageLabel);
+		return composite;
+	}
+
+}
