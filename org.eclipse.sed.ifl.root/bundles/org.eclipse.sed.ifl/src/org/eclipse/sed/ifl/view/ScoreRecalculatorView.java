36258645d9670b41227718ec05d1e4bf3d0a9560
kisdom <kissdominik@gmail.com>
2020-05-05 23:01:59 +0200

    Appending ScoreRecalculatorView class with basic methods

--- org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/view/ScoreRecalculatorView.java
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/view/ScoreRecalculatorView.java
@@ -1,5 +1,7 @@
 package org.eclipse.sed.ifl.view;
 
+import org.eclipse.sed.ifl.util.event.INonGenericListenerCollection;
+import org.eclipse.sed.ifl.util.event.core.NonGenericListenerCollection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
@@ -11,6 +13,19 @@ public class ScoreRecalculatorView extends View {
 	public ScoreRecalculatorView() {
 		dialog = new FileDialog(new Shell(), SWT.OPEN);
 		dialog.setFilterExtensions(new String[] {"*.csv"});
+	}
+	
+private NonGenericListenerCollection<String> fileSelected = new NonGenericListenerCollection<>();
+	
+	public INonGenericListenerCollection<String> eventFileSelected() {
+		return fileSelected;
+	}
+	
+	public void select() {
+		String path = dialog.open();
+		if (path != null) {
+			fileSelected.invoke(path);
+		}
 	}
 
 }
