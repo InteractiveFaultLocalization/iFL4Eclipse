7c6ab564eaa9835f0de7e04ee1824bc4ee9ee7eb
kisdom <kissdominik@gmail.com>
2020-05-05 23:17:20 +0200

    made basic skeleton for ScoreRecalculatorControl class

--- org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreRecalculatorControl.java
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreRecalculatorControl.java
@@ -2,6 +2,7 @@ package org.eclipse.sed.ifl.control.score;
 
 import org.eclipse.sed.ifl.control.Control;
 import org.eclipse.sed.ifl.model.score.ScoreListModel;
+import org.eclipse.sed.ifl.util.event.IListener;
 import org.eclipse.sed.ifl.view.ScoreLoaderView;
 
 public class ScoreRecalculatorControl extends Control<ScoreListModel, ScoreLoaderView> {
@@ -14,6 +15,49 @@ public class ScoreRecalculatorControl extends Control<ScoreListModel, ScoreLoade
 	
 	public void load() {
 		getView().select();
+	}
+	
+	public class Entry {
+		private String name;
+		private String detailsLink;
+		private boolean interactivity;
+		
+		public Entry(String name, String detailsLink, boolean interactivity) {
+			super();
+			this.name = name;
+			this.detailsLink = detailsLink;
+			this.interactivity = interactivity;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getDetailsLink() {
+			return detailsLink;
+		}
+		
+		public boolean isInteractive() {
+			return interactivity;
+		}
+	}
+	
+	private IListener<String> fileSelectedListener = new IListener<String>() {
+		public void invoke(String event) {
+			
+		}
+	};
+	
+	@Override
+	public void init() {
+		getView().eventFileSelected().add(fileSelectedListener);
+		super.init();
+	}
+	
+	@Override
+	public void teardown() {
+		getView().eventFileSelected().remove(fileSelectedListener);
+		super.teardown();
 	}
 
 }
