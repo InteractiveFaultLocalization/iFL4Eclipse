34026f962be724f45f35bf2e33bc5ad78bd28342
kisdom <kissdominik@gmail.com>
2020-05-13 08:22:56 +0200

    fixed setView in ScoreRecalculatorControl

--- org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreRecalculatorControl.java
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreRecalculatorControl.java
@@ -4,24 +4,36 @@ import org.eclipse.sed.ifl.control.Control;
 import org.eclipse.sed.ifl.model.score.ScoreListModel;
 import org.eclipse.sed.ifl.util.event.IListener;
 import org.eclipse.sed.ifl.view.ScoreLoaderView;
+import org.eclipse.sed.ifl.view.ScoreRecalculatorView;
 
 public class ScoreRecalculatorControl extends Control<ScoreListModel, ScoreLoaderView> {
-	
+
 	private boolean interactivity;
+	
+	private ScoreRecalculatorView view;
+
+	
 	
 	public ScoreRecalculatorControl(boolean interactivity) {
 		this.interactivity = interactivity;
 	}
-	
+
 	public void load() {
 		getView().select();
 	}
 	
+	
+	public void setView(ScoreRecalculatorView scoreRecalculatorView) {
+		this.view = view;
+		
+	}
+
+
 	public class Entry {
 		private String name;
 		private String detailsLink;
 		private boolean interactivity;
-		
+
 		public Entry(String name, String detailsLink, boolean interactivity) {
 			super();
 			this.name = name;
@@ -36,31 +48,32 @@ public class ScoreRecalculatorControl extends Control<ScoreListModel, ScoreLoade
 		public String getDetailsLink() {
 			return detailsLink;
 		}
-		
+
 		public boolean isInteractive() {
 			return interactivity;
 		}
 	}
-	
+
 	private IListener<String> fileSelectedListener = new IListener<String>() {
-		
-		
+
 		public void invoke(String event) throws UnsupportedOperationException {
 			throw new UnsupportedOperationException("Function is not yet implemented");
-			
+
 		}
 	};
-	
+
 	@Override
 	public void init() {
 		getView().eventFileSelected().add(fileSelectedListener);
 		super.init();
 	}
-	
+
 	@Override
 	public void teardown() {
 		getView().eventFileSelected().remove(fileSelectedListener);
 		super.teardown();
 	}
+
+	
 
 }
