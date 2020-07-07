06b3d71deb89527fdb8ce4c2a242121e90ea5500
kisdom <kissdominik@gmail.com>
2020-07-08 01:31:27 +0200

    current state

--- org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreRecalculatorControl.java
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreRecalculatorControl.java
@@ -1,6 +1,7 @@
 package org.eclipse.sed.ifl.control.score;
 
-import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.*;
+import org.apache.maven.model.*;
 
 import org.eclipse.sed.ifl.control.ViewlessControl;
 import org.eclipse.sed.ifl.model.score.ScoreListModel;
@@ -27,7 +28,6 @@ import org.eclipse.jdt.core.IJavaModel;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.launching.*;
-import org.eclipse.m2e.actions.MavenLaunchConstants;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.IMavenProjectRegistry;
@@ -42,12 +42,23 @@ public class ScoreRecalculatorControl extends ViewlessControl<ScoreListModel> {
 		this.currentIProject = selectedProject.getProject();
 	}
 
-	public void recalculate() throws UnsupportedOperationException, CoreException, IOException {
+	public void recalculate() throws Exception, UnsupportedOperationException, IOException {
 		System.out.println("Recalculating scores are requested...");
 		// throw new UnsupportedOperationException("Function is not yet implemented");
 
-		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager(); // Maven pluginnak és JUnitnak
-																					// lekérni
+		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+		MavenProjectBuilder mavenBuilder;// Maven pluginnak és JUnitnak
+		ProjectBuilderConfiguration mavenConfiger;
+		// lekérni
+
+		IMavenProjectRegistry projectManager = MavenPlugin.getMavenProjectRegistry();
+		IMavenProjectFacade projectFacade = projectManager.create(currentIProject, new NullProgressMonitor());
+		MavenProject mavenProject = projectFacade.getMavenProject(new NullProgressMonitor());
+		Model mvnModel = mavenProject.getModel();
+		Build mvnBuild = mavenProject.getBuild();
+
+		// getting MavenProject
+
 		ILaunchConfigurationType type = launchManager
 				.getLaunchConfigurationType("org.eclipse.m2e.Maven2LaunchConfigurationType");
 		ILaunchConfiguration[] configurations = launchManager.getLaunchConfigurations(type); // pluginokban van dedikált
@@ -71,8 +82,11 @@ public class ScoreRecalculatorControl extends ViewlessControl<ScoreListModel> {
 					skipTests = config.getAttribute("M2_SKIP_TESTS", false);
 					resolution = config.getAttribute("M2_WORKSPACE_RESOLUTION", false);
 					// Map<String, Object> lAttributes = config.getAttributes(); // a more elegant
-					// solution instead of above, still WIP
-					DebugUITools.launch(config, ILaunchManager.DEBUG_MODE);
+					// solution instead of above, still need to done properl
+
+					// DebugUITools.launch(config, ILaunchManager.DEBUG_MODE);
+
+					// goal: just saving the config instead of running it
 					break;
 				} else {
 					configuration.delete();
@@ -93,14 +107,12 @@ public class ScoreRecalculatorControl extends ViewlessControl<ScoreListModel> {
 			}
 		}
 
-		/*
-		 * if(isJREConfigured == false) { throw new
-		 * CoreException(StatusUtil.error(ScoreRecalculatorControl.class, "Project " +
-		 * currentIProject.getName() + " does not have JRE configured.")); }
-		 */
-		// Unsure about proper CodeException handling
+		if (isJREConfigured == false) {
+			throw new Exception("No JRE is configured");
+		}
 
 		IVMInstall jre = JavaRuntime.getDefaultVMInstall();
+		String jreVersion = jre.getName();
 		File jdkHome = jre.getInstallLocation();
 		String toolsPathString = Paths.get(jdkHome.getAbsolutePath() + "lib" + "tools.jar").normalize().toString();
 		IPath toolsPath = null;
@@ -108,30 +120,18 @@ public class ScoreRecalculatorControl extends ViewlessControl<ScoreListModel> {
 		IRuntimeClasspathEntry toolsEntry = JavaRuntime.newArchiveRuntimeClasspathEntry(toolsPath);
 		toolsEntry.setClasspathProperty(IRuntimeClasspathEntry.USER_CLASSES);
 
-		workingCopy.setAttribute(ILaunchManager.ATTR_PRIVATE, true);
-		workingCopy.setAttribute("ATTR_VM_INSTALL_NAME", jre.getName());
-		workingCopy.setAttribute("ATTR_VM_INSTALL_TYPE", jre.getVMInstallType().getId());
-		workingCopy.setAttribute("org.eclipse.jdt.launching.JRE_CONTAINER",
-				"org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/jre1.8.0_202");
-
 		String systemLibsPathString = Paths.get(JavaRuntime.JRE_CONTAINER).normalize().toString();
 		IPath systemLibsPath = null;
 		systemLibsPath.append(systemLibsPathString);
 		IRuntimeClasspathEntry systemLibsEntry = JavaRuntime.newRuntimeContainerClasspathEntry(systemLibsPath,
 				IRuntimeClasspathEntry.STANDARD_CLASSES);
 
-		IMavenProjectRegistry projectManager = MavenPlugin.getMavenProjectRegistry();
-		IMavenProjectFacade projectFacade = projectManager.create(currentIProject, new NullProgressMonitor());
-		MavenProject mavenProject = projectFacade.getMavenProject(new NullProgressMonitor());
-
 		String buildDirectory = mavenProject.getBuild().getDirectory();
 		String finalName = mavenProject.getBuild().getFinalName();
 		String finalArtifactPathString = buildDirectory + "/" + finalName + "." + mavenProject.getPackaging();
-		
+
 		IPath finalArtifactPath = null;
 		finalArtifactPath.append(finalArtifactPathString);
-
-		List classpath = new ArrayList();
 
 		IClasspathEntry[] projectClassPath = this.selectedProject.getRawClasspath();
 
@@ -139,12 +139,16 @@ public class ScoreRecalculatorControl extends ViewlessControl<ScoreListModel> {
 
 		String pomPath2 = this.currentIProject.getRawLocation().toOSString() + "/bundles/pom.xml";
 
-		System.out.println(pomPath);
-		System.out.println(pomPath);
+		List<Resource> mavenRes = mavenProject.getResources();
+		//mavenRes.add(toolsEntry.getMemento());
+		//mavenRes.add(systemLibsEntry.getMemento()); // It should work..
 		
-		
+		String pomPath3 = mavenRes.get(0).getDirectory() + "/bundles/pom.xml";
 
-		workingCopy.setAttribute("ATTR_CLASSPATH", classpath);
+		String pomPath4 = mvnBuild.getSourceDirectory() + "bundles/pom.xml";
+
+		workingCopy.setAttribute("ATTR_CLASSPATH", mavenRes);
+
 		workingCopy.setAttribute("ATTR_DEFAULT_CLASSPATH", false);
 
 		workingCopy.setAttribute("ATTR_MAIN_TYPE_NAME", typeName);
@@ -161,9 +165,18 @@ public class ScoreRecalculatorControl extends ViewlessControl<ScoreListModel> {
 
 		workingCopy.setAttribute("M2_ATTR_POM_DIR", pomPath2);
 
+		workingCopy.setAttribute("M2_ATTR_GOALS", goal);
+
 		workingCopy.setAttribute(RefreshUtil.ATTR_REFRESH_SCOPE, "${project}");
 
 		workingCopy.setAttribute(RefreshUtil.ATTR_REFRESH_RECURSIVE, true);
+
+		workingCopy.setAttribute(ILaunchManager.ATTR_PRIVATE, true);
+		workingCopy.setAttribute("ATTR_VM_INSTALL_NAME", jre.getName());
+		workingCopy.setAttribute("ATTR_VM_INSTALL_TYPE", jre.getVMInstallType().getId());
+		workingCopy.setAttribute("org.eclipse.jdt.launching.JRE_CONTAINER",
+				"org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/jre"
+						+ jreVersion);
 
 		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_JRE_CONTAINER_PATH, systemLibsPath.toString());
 
