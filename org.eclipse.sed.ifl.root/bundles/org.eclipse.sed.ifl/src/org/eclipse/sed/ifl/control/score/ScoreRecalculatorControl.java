5feda2b663dfe00f5139767a386165b4df71f78e
Ferenc Horváth <hferenc@inf.u-szeged.hu>
2021-08-03 14:12:01 +0200

    Added possible solutions for issues related to the score re-calculator.
    Some examples and suggestions were added for the following issues:
     - Retrieving the path to Java home for Maven invoker.
     - Resolving the project's output paths to calculate agent parameters.
     - Comparing versions of Maven artifacts.
    
    Also, minor things were refactored and a few todos were added.

--- org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreRecalculatorControl.java
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/control/score/ScoreRecalculatorControl.java
@@ -1,132 +1,96 @@
 package org.eclipse.sed.ifl.control.score;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.Arrays;
-import java.util.List;
-import java.util.StringJoiner;
-import java.util.regex.Pattern;
 
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.DefaultInvoker;
 import org.apache.maven.shared.invoker.InvocationRequest;
 import org.apache.maven.shared.invoker.Invoker;
 import org.apache.maven.shared.invoker.MavenInvocationException;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.sed.ifl.control.ViewlessControl;
+import org.eclipse.sed.ifl.ide.Activator;
 import org.eclipse.sed.ifl.ide.accessor.source.CodeEntityAccessor;
-import org.eclipse.sed.ifl.ide.accessor.source.WrongSelectionException;
-import org.eclipse.sed.ifl.ide.accessor.source.CodeEntityAccessor.Natures;
 import org.eclipse.sed.ifl.ide.modifier.source.PomModifier;
 import org.eclipse.sed.ifl.model.score.ScoreListModel;
-import org.eclipse.sed.ifl.util.exception.EU;
-import org.eclipse.ui.ISelectionService;
-import org.eclipse.ui.PlatformUI;
 
 
 public class ScoreRecalculatorControl extends ViewlessControl<ScoreListModel> {
 	
-	private final String instrumenterJar;
-	private final String javaHome;
-
-	public String getInstrumenterJar() {
-		return instrumenterJar;
-	}
-
-	public String getJavaHome() {
-		return javaHome;
-	}
+	private static final String CONFIGURED_POM_XML = "configuredPom.xml";
+	private final String instrumenterJar; // TODO: should be a text-like option in iFL preferences
+	private File javaHome;
+	private final IJavaProject project;
 	
 	public ScoreRecalculatorControl() {
-		this.instrumenterJar = "java-instrumenter-master" + File.separator + "target" + File.separator + "method-agent-0.0.4-jar-with-dependencies.jar";
-		this.javaHome = "C:/Program Files/Java/jdk1.8.0_202";
-		//this.javaHome = System.getenv("JAVA_HOME");
-	}
-	
-	private IResource extractSelection(ISelection sel) {
-	      if (!(sel instanceof IStructuredSelection))
-	         return null;
-	      IStructuredSelection ss = (IStructuredSelection) sel;
-	      Object element = ss.getFirstElement();
-	      if (element instanceof IResource)
-	         return (IResource) element;
-	      if (!(element instanceof IAdaptable))
-	         return null;
-	      IAdaptable adaptable = (IAdaptable)element;
-	      Object adapter = adaptable.getAdapter(IResource.class);
-	      return (IResource) adapter;
-	}
-	
-	//TODO Should this be in the codeEntityAccessor.java?
-	private IProject getSelectedProject() {
-		ISelectionService service = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getService(ISelectionService.class);
-		ISelection selection = service.getSelection();
-		if (selection != null) {
-			CodeEntityAccessor sourceAccessor = new CodeEntityAccessor(); 
-			//IResource resource = sourceAccessor.extractSelection(selection);
-			IResource resource = extractSelection(selection);
-			if (resource == null) {
-				throw new WrongSelectionException("Non-resources selected.");
-			}
-			IProject project = resource.getProject();
-			if (EU.tryUnchecked(() -> project.isNatureEnabled(Natures.JAVA.getValue()))) {
-				return project;
-			} else {
-				throw new WrongSelectionException("Non-Java project are selected.");
-			}
-		} else {
-			throw new WrongSelectionException("Nothing is selected.");
+//		this.instrumenterJar = "java-instrumenter-master" + File.separator + "target" + File.separator + "method-agent-0.0.4-jar-with-dependencies.jar";
+		this.instrumenterJar = "method-agent-0.0.4-jar-with-dependencies.jar"; 
+//		this.javaHome = "C:/Program Files/Java/jdk1.8.0_202";
+//		this.javaHome = System.getenv("JAVA_HOME");
+
+		project = new CodeEntityAccessor().getSelectedProject();
+		
+		try {
+			javaHome = JavaRuntime.getVMInstall(project).getInstallLocation();
+		} catch (CoreException e) { // if unable to determine the project's VM install
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (javaHome == null) {
+			javaHome = JavaRuntime.getDefaultVMInstall().getInstallLocation();
 		}
 	}
 	
+//	private URI getOutputLocation(IJavaProject project) throws JavaModelException {
+//		IPath outputLocation = project.getOutputLocation();
+//
+//		return ResourcesPlugin.getWorkspace().getRoot().findMember(outputLocation).getLocationURI();
+//	}
+	
 	public void recalculate() throws Exception {
-		IProject project = getSelectedProject();
+		final String jUnitVersion = "4.13";
+		final String projectName = project.getProject().getName();
+		final String pomPath = project.getProject().getRawLocation().append(CONFIGURED_POM_XML).toOSString();
 
-		String jUnitVersion = "4.13";
-		String projectName = project.getName();
-		String projectPath = project.getRawLocation().toOSString();
-		
-		String workspacePath = new File(projectPath).getParent();
-		
-		
 		final Job job = new Job(projectName + " coverage generation") {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
-				
 				try {					
 					// configuring the selected projet's pom
-					PomModifier modifier = new PomModifier(projectPath + File.separator + "pom.xml");
-					modifier.editSurefireConfig(workspacePath + File.separator + getInstrumenterJar(), "hu.szte.sed.JUnitRunListener");
+					PomModifier modifier = new PomModifier(project);
+					modifier.editSurefireConfig(instrumenterJar, "hu.szte.sed.JUnitRunListener");
 					modifier.updateJUnitVersion(jUnitVersion);
-					modifier.savePOM(projectPath + File.separator + "configuredPom.xml");
-					
+					modifier.savePOM(pomPath);
 					
 					// running maven test
 					InvocationRequest request = new DefaultInvocationRequest();
-					request.setPomFile( new File(projectPath + File.separator + "configuredPom.xml") );
-					request.setGoals( Arrays.asList("clean", "test -Dmaven.test.failure.ignore=true") );
+					request.setPomFile(new File(pomPath));
+					request.setGoals(Arrays.asList("clean", "test -Dmaven.test.failure.ignore=true"));
 					
-					request.setJavaHome( new File(getJavaHome()) );
+					request.setJavaHome(javaHome);
 					
 					Invoker invoker = new DefaultInvoker();
 					System.out.println("Executing request");
-					invoker.execute( request );
+					invoker.execute(request);
 				} catch (MavenInvocationException e) {
 					System.out.println("Request execution failed! (maven)");
 					e.printStackTrace();
-					return new Status(IStatus.ERROR, "uniqueIdentifier1", "Request execution failed! (maven)");
+					return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Request execution failed! (maven)");
 				} catch (Exception e1) {
-					// TODO Status-es... but how do they work?
 					e1.printStackTrace();
-					return new Status(IStatus.ERROR, "uniqueIdentifier2", "General exception trhown!");
+					return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "General exception thrown!");
 				}
 				return Status.OK_STATUS;
 			}
--- org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/ide/modifier/source/PomModifier.java
+++ org.eclipse.sed.ifl.root/bundles/org.eclipse.sed.ifl/src/org/eclipse/sed/ifl/ide/modifier/source/PomModifier.java
@@ -2,38 +2,49 @@ package org.eclipse.sed.ifl.ide.modifier.source;
 
 
 
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.apache.maven.model.*;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
-import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
-import org.codehaus.plexus.util.xml.Xpp3DomWriter;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.maven.artifact.versioning.ComparableVersion;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.DefaultModelWriter;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.codehaus.plexus.util.xml.Xpp3DomWriter;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.m2e.core.MavenPlugin;
+
 
 public final class PomModifier {
 
 	private final Xpp3Dom POM;
+	private final Model model;
+	private final MavenProject mavenProject;
+	private final IJavaProject javaProject;
 	private final String includeLine;
 	private final String excludeLine;
 
-	private Model ParsePOM(String POMPath) throws PomModificationException {
-		File POMFile = new File(POMPath);
-		MavenXpp3Reader reader = new MavenXpp3Reader();
-		try {
-			Model model = reader.read(new FileReader(POMFile)); // parsing XML
-			model.setPomFile(POMFile);
-			return model;
-		} catch (IOException e) {
-			throw new PomModificationException("File is not found." + POMPath);
-		} catch (XmlPullParserException e) {
-			throw new PomModificationException(e.getMessage());
-		}
-	}
+//	private Model ParsePOM(String POMPath) throws PomModificationException, CoreException {
+//		File POMFile = new File(POMPath);
+//		MavenXpp3Reader reader = new MavenXpp3Reader();
+//		try {
+//			Model model = reader.read(new FileReader(POMFile)); // parsing XML
+//			model.setPomFile(POMFile);
+//			return project.getModel();
+//		} catch (IOException e) {
+//			throw new PomModificationException("File is not found." + POMPath);
+//		} catch (XmlPullParserException e) {
+//			throw new PomModificationException(e.getMessage());
+//		}
+//	}
 	
 	private Xpp3Dom ParsePOMDom(String POMPath) throws PomModificationException {
 		File POMFile = new File(POMPath); 
@@ -48,13 +59,19 @@ public final class PomModifier {
 	    }
 	}
 	
-	public PomModifier(String POMPath) throws Exception {
-		Xpp3Dom dom = ParsePOMDom(POMPath);
-		Model model = ParsePOM(POMPath);
-		this.POM = dom;
-		//TODO replace with plexus utils
-		this.includeLine = model.getBuild().getOutputDirectory();
-		this.excludeLine = model.getBuild().getTestOutputDirectory();
+	public PomModifier(IJavaProject javaProject) throws Exception {
+		this.javaProject = javaProject;
+		
+//		mavenProject = MavenPlugin.getMaven().readProject(new File(POMPath), new NullProgressMonitor());
+		mavenProject = MavenPlugin.getMavenProjectRegistry().getProject(javaProject.getProject()).getMavenProject(new NullProgressMonitor());
+
+//		Model model = ParsePOM(POMPath);
+		model = mavenProject.getModel();
+//		POM = ParsePOMDom(project.);
+		POM = ParsePOMDom(mavenProject.getFile().getPath());
+		
+		includeLine = new File(model.getBuild().getOutputDirectory()).toURI().toString() + ".*";
+		excludeLine = new File(model.getBuild().getTestOutputDirectory()).toURI().toString() + ".*";
 	}
 	
 	public Xpp3Dom getPOM() {
@@ -69,7 +86,26 @@ public final class PomModifier {
 		return excludeLine;
 	}
 
-	public void updateJUnitVersion(String newVersion) {
+	public void updateJUnitVersion(String targetVersion) {
+//		Dependency dep = mavenProject.getModel().getDependencies().stream().filter(d -> "junit".equals(d.getArtifactId()) && "junit".equals(d.getGroupId())).findAny().get();
+//		dep.setVersion("4.13");
+//		try {
+//			String newPom = mavenProject.getFile().getPath().replace("pom.xml", "ize.xml");
+//			
+////			project.writeModel(new FileWriter(newPom));
+//			
+////			DefaultModelWriter dmw = new DefaultModelWriter();
+////			dmw.write(new File(newPom), null, project.getModel());
+//			
+//			IFile f = javaProject.getProject().getFile("ize.xml");
+//			if (f.exists()) {
+//				f.delete(true, new NullProgressMonitor());
+//			}
+//			MavenPlugin.getMavenModelManager().createMavenModel(f, mavenProject.getModel());
+//		} catch (CoreException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		Xpp3Dom dependencies = getElementsChild(this.POM, "dependencies");
 		Xpp3Dom junit = getElementsChildByArtifactId(dependencies, "junit");
 		if (junit == null) {
@@ -82,7 +118,7 @@ public final class PomModifier {
 			
 			groupdId.setValue("junit");
 			artifactId.setValue("junit");
-			version.setValue(newVersion);
+			version.setValue(targetVersion);
 			scope.setValue("test");
 			
 			junit.addChild(groupdId);
@@ -94,30 +130,57 @@ public final class PomModifier {
 			return;
 		}
 		
-		Xpp3Dom version = getElementsChild(junit, "version");
-		String oldVersion = version.getValue();
-		String majorOldVersionString = oldVersion.split("\\.")[0];
-		String minorOldVersionString = oldVersion.split("\\.")[1];
-		String majorNewVersionString = newVersion.split("\\.")[0];
-		String minorNewVersionString = newVersion.split("\\.")[1];
-		int majorOldVersion = Integer.parseInt(majorOldVersionString);
-		int minorOldVersion = Integer.parseInt(minorOldVersionString);
-		int majorNewVersion = Integer.parseInt(majorNewVersionString);
-		int minorNewVersion = Integer.parseInt(minorNewVersionString);
-		if (majorOldVersion < majorNewVersion) { // checking if version is lower, than 4.13
-			version.setValue(newVersion);
-		} else if (majorOldVersion >= majorNewVersion && minorOldVersion < minorNewVersion) {
-			version.setValue(newVersion);
+		Xpp3Dom currentVersion = getElementsChild(junit, "version");
+//		String oldVersion = version.getValue();
+//		String majorOldVersionString = oldVersion.split("\\.")[0];
+//		String minorOldVersionString = oldVersion.split("\\.")[1];
+//		String majorNewVersionString = newVersion.split("\\.")[0];
+//		String minorNewVersionString = newVersion.split("\\.")[1];
+//		int majorOldVersion = Integer.parseInt(majorOldVersionString);
+//		int minorOldVersion = Integer.parseInt(minorOldVersionString);
+//		int majorNewVersion = Integer.parseInt(majorNewVersionString);
+//		int minorNewVersion = Integer.parseInt(minorNewVersionString);
+//		if (majorOldVersion < majorNewVersion) { // checking if version is lower, than 4.13
+//			version.setValue(newVersion);
+//		} else if (majorOldVersion >= majorNewVersion && minorOldVersion < minorNewVersion) {
+//			version.setValue(newVersion);
+//		}
+		if (isCurrentOlder(currentVersion.getValue(), targetVersion)) {
+			currentVersion.setValue(targetVersion);
 		}
 	}
-		
+
+	private boolean isCurrentOlder(String current, String target) {
+		// org.apache.maven.artifact.versioning.DefaultArtifactVersion is also a good candidate for comparison
+		ComparableVersion currentVersion = new ComparableVersion(current);
+		ComparableVersion targetVersion = new ComparableVersion(target);
+
+		return currentVersion.compareTo(targetVersion) < 0;
+	}
+
 	public void editSurefireConfig(String agentFilePath, String listener) {
+		char separator = '=';
+		StringBuilder arglineBuilder = new StringBuilder();
+		arglineBuilder.append("-javaagent:").append(agentFilePath);
+
+		if (includeLine != null && !includeLine.isEmpty()) {
+			arglineBuilder.append(separator).append("includes=").append(includeLine);
+
+			separator = ',';
+		}
+
+		if (excludeLine != null && !excludeLine.isEmpty()) {
+			arglineBuilder.append(separator).append("excludes=").append(excludeLine);
+
+			separator = ',';
+		}
+
 		//String arglineValue = "-javaagent:" + agentFilePath + "=includes=" + (this.includeLine == null ? ""                : this.includeLine ) + ".*,excludes=" + (this.excludeLine == null ? "" : this.excludeLine + ".*");
-		String arglineValue = "-javaagent:" + agentFilePath + "=includes=.*org.*";// + (this.includeLine == null ? ""                : this.includeLine ) + ".*org.joda.time.*";//,excludes=" + (this.excludeLine == null ? "" : this.excludeLine + ".*");
+//		String arglineValue = "-javaagent:" + agentFilePath + "=includes=" + includeLine;// + (this.includeLine == null ? ""                : this.includeLine ) + ".*org.joda.time.*";//,excludes=" + (this.excludeLine == null ? "" : this.excludeLine + ".*");
 		//String arglineValue = "-javaagent:" + agentFilePath + "=includes=" + (this.includeLine == null ? ".*org.joda.time" : ".*org.joda.time") + ".*,excludes=" + (this.excludeLine == null ? "" : this.excludeLine + ".*");
 		//ez az aslo mukodik, a kozepso a test, a felso meg, ahogy talan ki kene nezzen
-		
-		
+		String arglineValue = arglineBuilder.toString();
+
 		Xpp3Dom mBuild = getElementsChild(this.POM, "build");
 		Xpp3Dom pManagement = getElementsChild(mBuild, "pluginManagement");
 		Xpp3Dom plugins = getElementsChild(pManagement, "plugins");
@@ -125,9 +188,9 @@ public final class PomModifier {
 		
 		if (surefire == null) {
 			surefire = createNode(plugins, "plugin");
-			createNode(surefire, "groupId", "org.apache.maven.plugins");
-			createNode(surefire, "artifactId", "maven-surefire-plugin");
-			createNode(surefire, "version", "3.0.0-M3");
+			createNodeWithValue(surefire, "groupId", "org.apache.maven.plugins");
+			createNodeWithValue(surefire, "artifactId", "maven-surefire-plugin");
+			createNodeWithValue(surefire, "version", "3.0.0-M3");
 		}
 		
 		Xpp3Dom configuration = getElementsChild(surefire, "configuration");
@@ -206,7 +269,7 @@ public final class PomModifier {
 		return newNode;
 	}
 	
-	private void createNode(Xpp3Dom parentNode, String name, String value) {
+	private void createNodeWithValue(Xpp3Dom parentNode, String name, String value) {
 		Xpp3Dom newNode = createNode(parentNode, name);
 		newNode.setValue(value);
 	}
